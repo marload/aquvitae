6229e5fa673ee2c73a6f26bd24121df6d4569969
marload <rladhkstn8@gmail.com>
2020-06-27 22:09:26 +0900

    versioning

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="aquvitae",
-    version="0.1.0",
+    version="0.1.1",
     description="The easiest Knowledge Distillation library for Light Weight DeepLearning",
     author="marload",
     author_email="rladhkstn8@gmail.com",
